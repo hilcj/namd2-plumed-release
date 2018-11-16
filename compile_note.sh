2790b6d8d51a32c6f9168af02ad41de492c0dfbe
hilcj <desmond@ME-VIOLILAB30.engin.umich.edu>
2018-11-16 15:57:54 -0500

    update

---
+++ compile_note.sh
@@ -0,0 +1,126 @@
+# On v23 do:
+# $   scp -r ~/apps/release vxx:~/apps/
+
+APPDIR=${HOME}/apps/
+LOCAL_DIR=${HOME}/.local
+mkdir $APPDIR
+
+# 1. Compile plumed.
+PLUMED=plumed-2.4.2
+PLUMED_SOURCE=$PLUMED
+cd $APPDIR
+mkdir $PLUMED
+cd $PLUMED
+
+# Deploy source file.
+cp ${HOME}/apps/release/${PLUMED_SOURCE}.tgz .
+tar xf ${PLUMED_SOURCE}.tgz
+cd $PLUMED_SOURCE
+
+# Build
+./configure --disable-mpi --prefix ${HOME}/apps/${PLUMED}/build | grep matheval
+# CHECK whether matheval is there or not.
+
+make -j8 && make install
+cp ${HOME}/apps/release/namd_plumed_patch/* ${HOME}/apps/${PLUMED}/build/lib/plumed/patches/
+
+# Load plumed into the working environment.
+cp ${HOME}/apps/release/plumed_enable ${LOCAL_DIR}/bin/
+source plumed_enable
+
+# 2.1 Compile NAMD2 with cuda.
+NAMD=namd-2.13
+NAMD_SOURCE=NAMD_2.13_Source
+cd $APPDIR
+mkdir $NAMD
+cd $NAMD
+
+# Deploy source file.
+cp ${HOME}/apps/release/${NAMD_SOURCE}.tar.gz .
+tar xf ${NAMD_SOURCE}.tar.gz
+cd $NAMD_SOURCE
+
+# Compile charmm
+tar -xf charm*
+cd charm*
+./build charm++ multicore-linux-x86_64 --with-production
+cd ..
+
+# Download tcl
+wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.5.9-linux-x86_64.tar.gz
+wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.5.9-linux-x86_64-threaded.tar.gz
+tar xzf tcl8.5.9-linux-x86_64.tar.gz
+tar xzf tcl8.5.9-linux-x86_64-threaded.tar.gz
+mv tcl8.5.9-linux-x86_64 tcl
+mv tcl8.5.9-linux-x86_64-threaded tcl-threaded
+
+# Configure namd2
+nano arch/Linux-x86_64.fftw3
+nano arch/Linux-x86_64.cuda
+
+# Compile and deploy
+./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-cuda --with-fftw3
+cd Linux-x86_64-g++
+make -j16;
+cd ../
+mv Linux-x86_64-g++ ../build_cuda;
+cd ../
+ln build_cuda/namd2 ${LOCAL_DIR}/bin/namd2-cuda
+
+# 2.2 Compile NAMD2 with plumed and cuda.
+# Fast version, assume 2.1 is done.
+cd $APPDIR
+cd $NAMD
+cd $NAMD_SOURCE
+./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-cuda --with-fftw3
+plumed patch -p -e namd-2.13
+cd Linux-x86_64-g++
+make -j16;
+cd ../
+mv Linux-x86_64-g++ ../build_plumed_cuda;
+cd ../
+ln build_plumed_cuda/namd2 ${LOCAL_DIR}/bin/namd2-plumed-cuda
+
+
+# Complete version
+/*
+NAMD=namd-2.13b1
+NAMD_SOURCE=NAMD_2.13b1_Source
+cd $APPDIR
+mkdir $NAMD
+cd $NAMD
+
+# Deploy source file.
+cp ${HOME}/apps/release/${NAMD_SOURCE}.tar.gz .
+tar xf ${NAMD_SOURCE}.tar.gz
+cd $NAMD_SOURCE
+
+# Compile charmm
+tar -xf charm*
+cd charm*
+./build charm++ multicore-linux-x86_64 --with-production
+cd ..
+
+# Download tcl
+  wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.5.9-linux-x86_64.tar.gz
+  wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.5.9-linux-x86_64-threaded.tar.gz
+  tar xzf tcl8.5.9-linux-x86_64.tar.gz
+  tar xzf tcl8.5.9-linux-x86_64-threaded.tar.gz
+  mv tcl8.5.9-linux-x86_64 tcl
+  mv tcl8.5.9-linux-x86_64-threaded tcl-threaded
+
+# Configure namd2
+nano arch/Linux-x86_64.fftw3
+nano arch/Linux-x86_64.cuda
+
+# Compile and deploy
+./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-cuda --with-fftw3
+plumed patch -p -e namd-2.13
+cd Linux-x86_64-g++
+make -j4
+cd ../
+mv Linux-x86_64-g++ ../build_plumed_cuda
+cd ../
+ln build_plumed_cuda/namd2 ${LOCAL_DIR}/bin/namd2-plumed-cuda
+
+
---
+++ plumed-2.4.2/src/tools/Exception.cpp
@@ -0,0 +1,96 @@
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   Copyright (c) 2012-2018 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+#include "Exception.h"
+
+#if defined(__PLUMED_HAS_EXECINFO)
+#include <execinfo.h>
+#endif
+
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+namespace PLMD {
+
+std::string Exception::trace() {
+  std::string message="\n\n********** STACK DUMP **********\n";
+#ifdef __PLUMED_HAS_EXECINFO
+  void* callstack[128];
+  int frames = backtrace(callstack, 128);
+  char** strs = backtrace_symbols(callstack, frames);
+  for (int i = 0; i < frames; ++i) {message+=strs[i]; message+="\n";}
+  free(strs);
+#else
+  message+="not available\n";
+#endif
+  message+="********** END STACK DUMP **********\n";
+  return message;
+}
+
+std::string Exception::format(const std::string&msg,const std::string&file,unsigned line,const std::string&function) {
+  std::string message;
+  if(getenv("PLUMED_STACK_TRACE"))message+=trace();
+  message+="\n+++ Internal PLUMED error";
+  if(file.length()>0) {
+    char cline[1000];
+    sprintf(cline,"%u",line);
+    message += "\n+++ file "+file+", line "+cline;
+    if(function.length()>0) message +=", function "+function;
+  }
+  if(msg.length()>0) message +="\n+++ message: "+msg;
+  return message;
+}
+
+
+Exception::Exception():
+  stackString(trace()),
+  msg(format("","",0,""))
+{
+  abortIfExceptionsAreDisabled();
+}
+
+Exception::Exception(const std::string&msg):
+  stackString(trace()),
+  msg(format(msg,"",0,""))
+{
+  abortIfExceptionsAreDisabled();
+}
+
+Exception::Exception(const std::string&msg,const std::string&file,unsigned line,const std::string&function):
+  stackString(trace()),
+  msg(format(msg,file,line,function))
+{
+  abortIfExceptionsAreDisabled();
+}
+
+void Exception::abortIfExceptionsAreDisabled() {
+#if ! defined(__PLUMED_HAS_EXCEPTIONS)
+  fprintf(stderr,"%s","Exceptions are disabled, aborting now\n");
+  fprintf(stderr,"%s",Exception::what());
+  fprintf(stderr,"\n");
+  std::abort();
+#endif
+}
+
+}
+
+
